0b74102b26ac1fd979a888e6a8d5adfa5de79baf
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-26 12:06:22 -0300

    feat: added customer repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,16 +1,23 @@
 import { container } from "tsyringe";
 import type { DataSource } from "typeorm";
+import type { ICustomersRepository } from "@/modules/customers/domain/repositories/ICustomersRepository";
+import { CustomersRepository } from "@/modules/customers/infra/CustomersRepository";
 import type { IUserRepository } from "../../modules/users/domain/repositories/IUserRepository";
 import { UserRepository } from "../../modules/users/infra/repositories/UserRepository";
 import AppDataSource from "../providers/typeorm/index";
 
 export async function registerSharedProviders(): Promise<void> {
-	const dataSource: DataSource = await AppDataSource();
+    const dataSource: DataSource = await AppDataSource();
 
-	container.registerInstance<DataSource>("DataSource", dataSource);
+    container.registerInstance<DataSource>("DataSource", dataSource);
 
-	container.registerSingleton<IUserRepository>(
-		"UserRepository",
-		UserRepository,
-	);
+    container.registerSingleton<IUserRepository>(
+        "UserRepository",
+        UserRepository
+    );
+
+    container.registerSingleton<ICustomersRepository>(
+        "CustomersRepository",
+        CustomersRepository
+    );
 }
