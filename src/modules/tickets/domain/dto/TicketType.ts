c55ab78f007373bf545be316fe60db9923cfdb7e
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-29 22:03:29 -0300

    feat: add enum ticket types

---
+++ src/modules/tickets/domain/dto/TicketType.ts
@@ -0,0 +1,13 @@
+export enum TicketType {
+    Instalacao = "instalacao_completa",
+    InstalacaoComTreinamento = "instalacao_com_treinamento",
+    ReinstalacaoSistema = "reinstalacao_sistema",
+
+    CorrigirParametrizarSistema = "corrigir_parametrizar_sistema",
+    ErrorImpressao = "error_impressao",
+    Tef = "tef",
+    ProblemasHardware = "problemas_hardware",
+    ProblemasPBMS = "problemas_pbms",
+    VisitaPresencial = "visita_presencial",
+    PedidoNovasFuncoesSistema = "pedido_novas_funcoes_sistema",
+}
